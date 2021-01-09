9f8446c2f96a3344319293c1487d58d93f48dea7
Aram Ebtekar <EbTech@users.noreply.github.com>
2021-01-09 04:24:36 -0800

    Set up postgres database

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, HttpResponse};
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+/*async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}*/
